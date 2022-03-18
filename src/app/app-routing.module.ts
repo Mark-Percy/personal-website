de4656f876d66e503fcdf7cc77151697011f9601
Mark Percy <mark.percy98@outlook.com>
2022-03-18 17:13:12 +0000

    Set up of some components that will be used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './personal/projects/projects.component';
+import { TimelineComponent } from './personal/timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:AboutComponent, pathMatch:'full'},
+  {path:'timeline', component:TimelineComponent},
+  {path:'projects', component:ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
